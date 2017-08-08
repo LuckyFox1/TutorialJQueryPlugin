ce89380d6b2c13e1ac7324e1da3d075dbc08f625
LuckyFox1 <ms.katyaaa13@mail.ru>
2017-08-08 11:03:33 +0300

    Create basic pattern (JQuery plugins)

---
+++ plugin.js
@@ -0,0 +1,34 @@
+;(function ( $, window, document, undefined ) {
+    var pluginName = 'defaultPluginName',
+        defaults = {
+            propertyName: "value"
+        };
+
+    function Plugin( element, options ) {
+        this.element = element;
+        this.options = $.extend({}, defaults, options) ;
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        this.init();
+    }
+
+    Plugin.prototype.init = function () {
+        this.options.element.click(function () {
+            alert('task #144');
+         });
+         $(this.element).resize(function () {
+             console.log(this.innerWidth);
+         });
+    };
+
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName,
+                    new Plugin( this, options ));
+            }
+        });
+    }
+
+})( jQuery, window, document );
---
+++ script.js
@@ -0,0 +1,1 @@
+$(window).defaultPluginName({element: $('#1')});
